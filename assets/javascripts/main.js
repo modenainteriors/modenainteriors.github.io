3edd5fb51e30afd78d7346d57c1044e44d45437d
Aaron Ghani <aaronghani@gmail.com>
2014-12-30 13:16:36 +0000

    Create services nav and integrate with existing nav
    - Create services nav
    - Adjust main nav to link first services child page
    - Adjust javascript to handle active page when browsing a services child
      page

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -4,14 +4,24 @@ $(function () {
 
 function setActivePageInNav() {
 	var path = window.location.pathname;
-	
+	path = path.split("/")
 
-	$(".nav a").each(function () {
-		var href = $(this).attr('href');
+	$("#modena-main-nav .nav a").each(function () {
+		var href = $(this).attr('href').split('/');
+		console.log(path);
 		console.log(href);
-		if(path === href) {
+		if(path[1] === href[1]) {
 			$(this).parent().addClass('active');
-		}
+		} 	
 	})
 
+	node = $("#modena-services-nav")
+	if ( document.body.contains(node[0]) ) {
+		$("#modena-services-nav .nav a").each(function() {
+			var href = $(this).attr('href').split('/');
+			if(path[2] === href[2]) {
+				$(this).parent().addClass('active');
+			}
+		})
+	}	
 }
